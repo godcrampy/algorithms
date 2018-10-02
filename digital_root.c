d3f5c537b913526e1164c7ed6a2760a8a6d99cd2
Sahil Bondres <42311546+godcrampy@users.noreply.github.com>
2018-10-02 09:44:48 +0530

    Add files via upload

---
+++ digital_root.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+void main(){
+  int n,s=0;
+  puts("Enter Number: ");
+  scanf("%d",&n);
+  while(n>9){
+    while(n>=1){
+      s+=n%10;n/=10;
+    }
+    n=s;
+    s=0;
+  }
+printf("\n%d",n);
+}
