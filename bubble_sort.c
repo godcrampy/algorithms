0fc9b409943085fdded51b9e507a68f9adc5d40e
Sahil Bondres <42311546+godcrampy@users.noreply.github.com>
2018-10-07 14:48:06 +0530

    Add files via upload

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 void main(){
+  //Take Array
 puts("Enter Number\n");
 int n,i;
 scanf("%d",&n);
@@ -8,8 +9,13 @@ puts("Enter Array\n");
 for(i=0;i<n;i++){
   scanf("%d",&a[i]);
 }
+//Sort Array
 puts("sorting\n");
-int t,c=1,b=n;
+int t,c=1,b=n-1;
+// t is used for swapping
+//b is used for keeping track pf how much to bubble in one iteration
+// b is n-1 coz i will reach till n-2 then whic will be the second last number of the array
+// c is couter to keep track when the array is sorted and no more swaps are required
 while(c!=0){
   c=0;
   for(i=0;i<b;i++){
